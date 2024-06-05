bd74ed7bb34d7bc8ce898833d027096d60deb720
Emil Isabekov <44045464+Lime2404@users.noreply.github.com>
2024-06-05 18:14:33 +0400

    PhoneClass1

--- src/main/java/Part1/Lesson1/Phone.java
+++ src/main/java/Part1/Lesson1/Phone.java
@@ -1,4 +1,32 @@
 package Part1.Lesson1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Phone {
+    int number;
+    String model;
+    Double weight;
+
+    public Phone(int number, String model, Double weight) {
+        this.number = number;
+        this.model = model;
+        this.weight = weight;
+    }
+
+    public static void main(String[] args) {
+        Phone iphone = new Phone(15, "Pro", 200.20);
+        Phone xiaomi = new Phone(5, "Pro", 200.30);
+        Phone nokia = new Phone(6300, "brick", 200.40);
+
+        List<Phone> phones = new ArrayList<>();
+        phones.add(iphone);
+        phones.add(xiaomi);
+        phones.add(nokia);
+
+        for(Phone p: phones){
+            System.out.println("Характеристики iphone: " + " версия: "+ p.number + ", модель: " + p.model + ", вес в граммах: " + p.weight);
+        }
+    }
+
 }
