154d3533ef16cf9dfd5980df4c3b393e6518d9c0
Emil Isabekov <44045464+Lime2404@users.noreply.github.com>
2024-06-07 11:51:46 +0400

    SmallestNumber

---
+++ src/main/java/Part1/Lesson2/SmallerNumber.java
@@ -0,0 +1,21 @@
+package Part1.Lesson2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SmallerNumber {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        List<Integer> data = new ArrayList<>();
+        for(int i = 0; i < 3; i++){
+            data.add(Integer.parseInt(reader.readLine()));
+        }
+        System.out.println("The smalles numner is " + Collections.min(data));
+    }
+}
