814a95ea623f5e973a1c1f265e7d7806f1d5977f
Emil Isabekov <44045464+Lime2404@users.noreply.github.com>
2024-06-10 10:22:16 +0400

    PrintLoop

---
+++ src/main/java/Part1/Lesson2/WhileLoop.java
@@ -0,0 +1,11 @@
+package Part1.Lesson2;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i = 1;
+        while(i <= 10){
+            System.out.println("Task" + i);
+            i++;
+        }
+    }
+}
