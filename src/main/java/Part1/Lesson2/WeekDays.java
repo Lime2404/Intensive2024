8f5ee165431a5b9d789d7ed88a7ea144a79223cb
Emil Isabekov <44045464+Lime2404@users.noreply.github.com>
2024-06-10 05:20:15 +0400

    WeekDaysValue

--- src/main/java/Part1/Lesson2/WeekDays.java
+++ src/main/java/Part1/Lesson2/WeekDays.java
@@ -7,10 +7,44 @@ public class WeekDays {
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextInt()){
             int value = scanner.nextInt();
-            if(value > 0 && value < 10)
-                System.out.println("Positive number is less than 10 or zero");
-            else
-                System.out.println("Positive number is greater than 10 or negative.");
+            if(value >= 1 && value <= 7) {
+                // first option
+//                switch (value){
+//                    case 1:
+//                        System.out.println("Monday");
+//                        break;
+//                    case 2:
+//                        System.out.println("Tuesday");
+//                        break;
+//                    case 3:
+//                        System.out.println("Wednesday");
+//                        break;
+//                    case 4:
+//                        System.out.println("Thursday");
+//                        break;
+//                    case 5:
+//                        System.out.println("Friday");
+//                        break;
+//                    default:
+//                        System.out.println("Day off");
+//                        break;
+//
+//                }
+                //second
+                if (value == 1)
+                    System.out.println("Monday");
+                if(value == 2)
+                    System.out.println("Tuesday");
+                if(value == 3)
+                    System.out.println("Wednesday");
+                if(value == 4)
+                    System.out.println("Thursday");
+                if(value == 5)
+                    System.out.println("Friday");
+                else if(value==6 || value==7)
+                    System.out.println("Day off");
+
+            }
         }
         scanner.close();
     }
