ca366bc452ccb06e10d7e6ca2df5b883c8fc0cdc
Emil Isabekov <44045464+Lime2404@users.noreply.github.com>
2024-06-18 21:25:10 +0400

    Lesson3 beautified

--- src/main/java/Part1/Lesson3/OneDimensionalArray.java
+++ src/main/java/Part1/Lesson3/OneDimensionalArray.java
@@ -4,9 +4,12 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
+//A one-dimensional array of the String type.
 public class OneDimensionalArray {
     public static void main(String[] args) {
+//  1. Create an array of type String with size 7
         List<String> days = new ArrayList<>(7);
+//  2. Write down the values of the days of the week in it.
         days.add("Monday");
         days.add("Tuesday");
         days.add("Wednesday");
@@ -14,7 +17,7 @@ public class OneDimensionalArray {
         days.add("Friday");
         days.add("Saturday");
         days.add("Sunday");
-
+//  3. Display the value of the last item on the console
         System.out.println(days.get(6));
     }
 }
