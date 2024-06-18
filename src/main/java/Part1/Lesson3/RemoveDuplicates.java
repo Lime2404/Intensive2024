3a7c341c02def23933f51ee5142bb60b998e27cd
Emil Isabekov <44045464+Lime2404@users.noreply.github.com>
2024-06-18 22:18:49 +0400

    Lesson3 beautified

--- src/main/java/Part1/Lesson3/RemoveDuplicates.java
+++ src/main/java/Part1/Lesson3/RemoveDuplicates.java
@@ -5,28 +5,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+//Dialing numbers
 public class RemoveDuplicates {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String input = reader.readLine();
-        // 1,2,3,4,4,5
-
+//  1. The user enters a set of numbers as a single string 1,2,3,4,4,5
         String[] inputArray = input.split(",");
-
-        //  numberSet не даст хранить не уникальные знаяения
+//  2. Get rid of duplicate items in a row.
+        //Серег, содаю numberSet. тк он не даст хранить не уникальные знаяения
         Set<Integer> numberSet = new LinkedHashSet<>();
-
-
         for (String element : inputArray) {
             try {
                 int number = Integer.parseInt(element.trim());
                 numberSet.add(number);
-            } catch (NumberFormatException e) {
+            } catch (NumberFormatException e){
+                System.out.println("input contain non integer symbols");
             }
         }
-
         List<Integer> resultList = new ArrayList<>(numberSet);
-
+//  3. Display the result on the screen.
         System.out.println("Массив после удаления дубликатов:");
         for (int number : resultList) {
             System.out.print(number + " ");
--- src/main/java/Part1/Lesson3/SplittingCollection.java
+++ src/main/java/Part1/Lesson3/SplittingCollection.java
@@ -4,21 +4,26 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 
+//Splitting the collection
 public class SplittingCollection {
     public static void main(String[] args) {
+//  1. Create a collection of numbers from 0 to 50
         ArrayList<Integer> collection = new ArrayList<>(50);
         for(int i = 0; i < 50; i++){
             collection.add(i);
         }
         iterateElements(collection);
     }
-
+//  2. Write a method that iterates through the elements of a collection
     static void iterateElements(Collection<Integer> collection) {
+//  4. To iterate over the elements, use an iterator
         Iterator<Integer> iterator = collection.iterator();
         while (iterator.hasNext()) {
             int number = iterator.next();
+//  3. If the number is greater than 20 and less than 30, display them on the screen
             if (number > 20 && number < 30){
-                System.out.println(number); }
+                System.out.println(number);
+            }
         }
     }
 }
