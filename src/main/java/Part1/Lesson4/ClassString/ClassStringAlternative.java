1f10d1b5ea06c11fd206f77b524d8ed6ef8e76cb
Emil Isabekov <44045464+Lime2404@users.noreply.github.com>
2024-06-19 16:36:35 +0400

    Lesson4 beautified

--- src/main/java/Part1/Lesson4/ClassString/ClassStringAlternative.java
+++ src/main/java/Part1/Lesson4/ClassString/ClassStringAlternative.java
@@ -3,49 +3,60 @@ package Part1.Lesson4.ClassString;
 public class ClassStringAlternative {
 
     // Варианта в одном методе сделать, но как-то громоздко всё начинает выглядеть, поэтому я на отдельгые методы разбил требования
-            public static void validateInputData(String any) {
-                String[] favorites = {"I", "Java", "@", "!!", "like"};
-                for (String s : favorites) {
-                    if (any.contains(s)) {
-                        System.out.println("Input data contains famorite words");
-                        System.out.println("the last character of the line is " + any.charAt(any.length() - 1));
+    public static void validateInputData(String any) {
+        String[] favorites = {"I", "Java", "@", "!!", "like"};
+        for (String s : favorites) {
+            if (any.contains(s)) {
+                System.out.println("Input data contains famorite words");
+                System.out.println("the last character of the line is " + any.charAt(any.length() - 1));
 
-                        if (any.endsWith("!!")) {
-                            System.out.println("Input sting ends with \"!!\"");
-                        } else System.out.println("Input sting doesn't contain symbol \"!!\"");
+                if (any.endsWith("!!")) {
+                    System.out.println("Input sting ends with \"!!\"");
+                } else System.out.println("Input sting doesn't contain symbol \"!!\"");
 
-                        if (any.endsWith("!!")) {
-                            System.out.println("Input sting ends with \"!!\"");
-                        } else System.out.println("Input sting doesn't contain symbol \"!!\"");
+                if (any.endsWith("!!")) {
+                    System.out.println("Input sting ends with \"!!\"");
+                } else System.out.println("Input sting doesn't contain symbol \"!!\"");
 
-                        if (any.startsWith("I like")) {
-                            System.out.println("Input sting starts from \"I like\"");
-                        } else System.out.println("Input sting doesn't contain symbol \"!!\"");
+                if (any.startsWith("I like")) {
+                    System.out.println("Input sting starts from \"I like\"");
+                } else System.out.println("Input sting doesn't contain symbol \"!!\"");
 
-                        if (any.contains("Java")) {
-                            System.out.println("Input sting contain  \"Java\"");
-                        } else System.out.println("Input sting doesn't contain symbol \"Java\"");
+                if (any.contains("Java")) {
+                    System.out.println("Input sting contain  \"Java\"");
+                } else System.out.println("Input sting doesn't contain symbol \"Java\"");
 
-                        if (any.contains("Java")) {
-                            String substing = "Java";
-                            System.out.println("Еhe position of the substring \"Java\" is " + any.indexOf(substing));
-                        } else System.out.println("Input sting doesn't contain symbol \"Java\"");
-                        break;
-                    } else System.out.println("Input sting doesn't contain symbols");
-                    System.exit(1);
-                }
-            }
-            public static String replaceCharacter (String any,char oldChar, char newChar){
-                return any.replace(oldChar, newChar);
-            }
+                if (any.contains("Java")) {
+                    String substing = "Java";
+                    System.out.println("Еhe position of the substring \"Java\" is " + any.indexOf(substing));
+                } else System.out.println("Input sting doesn't contain symbol \"Java\"");
+                break;
+            } else System.out.println("Input sting doesn't contain symbols");
+            System.exit(1);
+        }
+    }
 
-       public static void main (String[]args){
-           String any = "I like Java!!";
-//           String any = "gtr";
+    public static String replaceCharacter(String any, char oldChar, char newChar) {
+        return any.replace(oldChar, newChar);
+    }
 
-           validateInputData(any);
+    public static String convertUpperCase(String any){
+        String convertedString = any.toUpperCase();
+        return convertedString;
+    }
+    //  9. Convert the string to lowercase.
+    public static String convertLowerCase(String any){
+        String convertedString = any.toLowerCase();
+        return convertedString;
+    }
 
-           String replacedString = replaceCharacter(any, 'a', 'o');
-           System.out.println(replacedString);
-       }
+    public static void main(String[] args) {
+        String any = "I like Java!!";
+//           String any = "test";
+        validateInputData(any);
+        String replacedString = replaceCharacter(any, 'a', 'o');
+        System.out.println(replacedString);
+        convertUpperCase(any);
+        convertLowerCase(any);
+    }
 }
