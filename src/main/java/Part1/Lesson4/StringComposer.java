6bfe8da3f78312afeca89c56eb8665f8e3af4d50
Emil Isabekov <44045464+Lime2404@users.noreply.github.com>
2024-06-19 16:42:24 +0400

    Lesson4 beautified

--- src/main/java/Part1/Lesson4/StringComposer.java
+++ src/main/java/Part1/Lesson4/StringComposer.java
@@ -1,9 +1,9 @@
 package Part1.Lesson4;
 
+//Compose a string using the StringBuilder class
 public class StringComposer {
 
     public static void main(String[] args) {
-
         int x = 3;
         int y = 56;
 
@@ -11,10 +11,13 @@ public class StringComposer {
         StringBuilder subtraction = new StringBuilder();
         StringBuilder multiplication = new StringBuilder();
 
+//  1.  Two numbers are given, for example, 3 and 56, you need to make the following lines: 3 + 56 = 59 3 – 56 = -53 3 * 56 = 168. Use the
+//      StringBuilder.append() method
         addition.append(x).append(" + ").append(y).append(" = ").append(x+y);
         subtraction.append(x).append(" - ").append(y).append( " = ").append(x-y);
         multiplication.append(x).append(" * ").append(y).append(" = ").append(x*y);
 
+//  2. Replace the "=" symbol with the word "equals". Use the StringBuilder.replace() methods.
         String replace_addition = addition.toString().replace(" = ", " equals ");
         String replace_subtraction = subtraction.toString().replace(" = ", " equals ");
         String replace_multiplication = multiplication.toString().replace(" = ", " equals ");
